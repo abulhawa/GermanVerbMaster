012ce4e3470c879eb8d0a4daa52e4698fbbb02cb
Ali <17217248+abulhawa@users.noreply.github.com>
2025-10-16 08:29:16 +0200

    Remove partner integration tables

---
+++ migrations/0013_remove_partner_rate_limit_tables.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS "integration_usage";
+--> statement-breakpoint
+DROP TABLE IF EXISTS "integration_partners";
+--> statement-breakpoint
+DROP TABLE IF EXISTS "rate_limit_counters";
